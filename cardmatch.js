3958d9bc282f69693e6232d6f81e32748bf6f02a
Ben-PC\BenJamin <benwildie@hotmail.com>
2016-09-11 22:08:06 +1000

    Added user class

--- cardmatch.js
+++ cardmatch.js
@@ -4,6 +4,7 @@ class Card{
 		this.imageurl=imageurl;
 		this.articleurl=articleurl;
 		this.excerpt=excerpt;
+		//0=neither card is drawn, 1=article card only 2 = image card onlr 3 = both have been drawn
 		this.check;			
 	}
 	imagecard() {
@@ -12,5 +13,18 @@ class Card{
 	articlecard() {
 		return [this.keyword, this.articleurl]
 	}
-	
+	imageused(){
+		if (this.check==1){this.check=3
+		}else{this.check=2}
+	}
+	articleused(){
+		if (this.check==2){this.check=3
+		}else{this.check=1}
+	}
+}
+class User{
+	constructor(username,upassword){
+		this.username = username;
+		this.upassword = upassword;
+	}
 }
