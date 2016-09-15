6f18f467ab775799c4f5b73675a34f529020a215
Ben-PC\BenJamin <benwildie@hotmail.com>
2016-09-15 11:05:44 +1000

    added more classes

--- cardmatch.js
+++ cardmatch.js
@@ -1,11 +1,11 @@
 class Card{
-	constructor(keyword,imageurl, articleurl,excerpt,check ){
+	constructor(keyword,imageurl, articleurl,excerpt){
 		this.keyword=keyword;
 		this.imageurl=imageurl;
 		this.articleurl=articleurl;
 		this.excerpt=excerpt;
 		//0=neither card is drawn, 1=article card only 2 = image card onlr 3 = both have been drawn
-		this.check;			
+		this.check = 0;			
 	}
 	imagecard() {
 		return [this.keyword, this.imageurl]
@@ -28,3 +28,28 @@ class User{
 		this.upassword = upassword;
 	}
 }
+class student extends User{
+	constructor(username,upassword,teacherID,favourites){
+			super(username,upassword);
+			this.teacherID=teacherID;
+			this.favourites=favourites;
+	}
+}
+
+class CardSet{
+	constructor(topic,cardlist){
+		this.topic=topic;
+		this.cardlist=cardlist;
+	}
+	drawhand(num){
+		int ran;
+		var hand=[];
+		for (int i=0; i<num; i++){
+			ran = Math.floor(Math.random() * cardlist.length);
+			hand.push(cardlist[ran]);
+			cardlist.splice(ran,1);
+		}
+		return hand;
+	}
+}
+
