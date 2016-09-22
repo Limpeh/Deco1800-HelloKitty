732968f381d9f1a7673a66e5fad395bebd9f062e
Ben-PC\BenJamin <benwildie@hotmail.com>
2016-09-22 13:05:17 +1000

    click confirm creates card object

--- cardmatch.js
+++ cardmatch.js
@@ -126,8 +126,9 @@ class game{
 ///////////////////////////////////////////////////////////////////////
 /****//****//****//****//****//****//****//****//****//****//****//***/
 	var keywordcards=[];
-	
+	var loadedArt = [];
 	var loadedImages = [];
+	var loadedEx = [];
     var urlPatterns = ["flickr.com", "nla.gov.au", "artsearch.nga.gov.au", "recordsearch.naa.gov.au", "images.slsa.sa.gov.au"];
     var found = 0;
 	var artic;
@@ -148,6 +149,12 @@ class game{
 	}
 	$("form#Confirm").submit(function(event) {
 		  event.preventDefault();
+		 var i = $("input[name='cardimage']:checked").val();
+		 newcard.imageurl= loadedImages[i];
+		i = $("input[name='cardart']:checked").val();
+		newcard.excerpt=loadedEx[i];
+		newcard.articleurl = loadedArt[i];
+		console.log(loadedEx[i]);
 		if (newcard.keyword!="null"){
 				keywordcards.push(newcard);
 			}
@@ -181,6 +188,7 @@ class game{
 
         });
 		 var searchTerm = $("#searchTerm").val();
+		 newcard.keyword=$("#searchTerm").val();
 			   var urlarticle = "http://api.trove.nla.gov.au/result?key=" 
 		        + apiKey + "&encoding=json&zone=" + searchZone 
 		        + "&sortby=dateDesc&q=" + searchTerm + "&callback=?";
@@ -220,7 +228,7 @@ function getArticleText(index, item){
 	// Pull what values you can from the original result - article title & URL
 	var title = item.heading;
 	var url = item.troveUrl;
-	
+	loadedArt.push(articleURL);
 	// Perform the search
 	$.getJSON(encodeURI(articleURL), function(data){
 		// Create a div element
@@ -240,10 +248,11 @@ function getArticleText(index, item){
 			);
 			//Add the result to the search results element
 			if (count<num){
-				$("#Articles").append(" <label><input type='radio' name='cardart' value='"+i+"'><<div class = 'A"+count+"'id='outputArt'></div><label/>");
+				$("#Articles").append(" <label><input type='radio' name='cardart' value='"+count+"'><div class = 'A"+count+"'id='outputArt'></div><label/>");
 				//$(".wrap").append("<div class = 'A"+count+"'id='outputArt'></div>");
 				$(".A"+count).append($(info));
 			}
+			loadedEx.push($(info));
 			count++;
 			newcard.articleURL=url;
 			newcard.excerpt=excerpt;
