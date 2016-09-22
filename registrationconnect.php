203896c739dcfa64b56d441a4588602b08abe37c
Weijie <flyingbicycle@hotmail.com>
2016-09-22 21:08:26 +1000

    changed login and registration to php versions to allow for php sync. 2) registration connect is the php code to connect the teacher_sign_up page to the database. not working at the moment tho

---
+++ registrationconnect.php
@@ -0,0 +1,30 @@
+<?php
+
+define ('DB_NAME','CardMatch');
+define ('DB_USER','s4396765')
+define ('DB_PASSWORD','sakaesushi1630')
+define ('DB_HOST','localhost');
+
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+
+if (!$link) {
+	die("Could not connect:" . mysql_error());
+}
+
+$db_selected = mysql_select_db(DB_NAME, $link);
+
+if (!$db_selected) {
+	die ('Can\'t use ' . DB_NAME . ':' . mysql_error()
+}
+
+$username = $_POST['username']
+$password = $_POST['password']
+
+$sql = "INSERT INTO TEACHER (username,password) VALUES ('$username', '$password')";
+
+if (!mysql_query($sql)) {
+	die("Error:" . mysql_error());
+}
+
+mysql_close();
+?>
---
+++ teacher_sign_up.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>CardMatch Main</title>
+</head>
+
+<div id="Banner">
+	<p><h1>CARDMATCH</h1></p>
+	<p><h3>The learning game</h3></p>
+</div>
+
+<body>
+
+<h2>Sign Up</h2>
+<form action ="registrationconnect.php" method ="post">
+	<div id="Student">
+		<p><label for="username">Username</label></p>
+		<p><input type="text" id="username" name="username" placeholder="Username"/></p>
+
+		<p><label for="password">Password</label></p>
+		<p><input type="text" id="password" name="password" placeholder="Password"/></p>
+
+	</div>
+			
+	<p><input type="submit" value="submit"></p>
+</form>
+	<p><a href="login.html">Back</a></p>
+
+<footer>
+</footer>
+
+</body>
+</html>
