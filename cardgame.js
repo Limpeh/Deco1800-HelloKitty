371d0baad7a41cc0721026da6ae8e71728a18cbc
Ben-PC\BenJamin <benwildie@hotmail.com>
2016-10-19 18:02:24 +1000

    additions made to game

---
+++ cardgame.js
@@ -0,0 +1,235 @@
+class Card{
+	constructor(keyword,imageurl, articleurl,excerpt){
+		this.keyword=keyword;
+		this.imageurl=imageurl;
+		this.articleurl=articleurl;
+		this.excerpt=excerpt;
+		//0=neither card is drawn, 1=article card only 2 = image card onlr 3 = both have been drawn
+		this.check = 0;			
+	}
+	imagecard() {
+		return [this.keyword, this.imageurl]
+	}
+	articlecard() {
+		return [this.keyword, this.articleurl]
+	}
+	imageused(){
+		if (this.check==1){this.check=3
+		}else{this.check=2}
+	}
+	articleused(){
+		if (this.check==2){this.check=3
+		}else{this.check=1}
+	}
+}
+$(document).ready(function(){
+	$("#outputArt").click(function() {
+		$("#outputArt").css("background-color", "blue");
+	});
+	$("#outputImg").click(function() {
+		$("#outputImg").css("background-color", "blue");
+	});
+});
+class User{
+	constructor(username,upassword){
+		this.username = username;
+		this.upassword = upassword;
+	}
+}
+class student extends User{
+	constructor(username,upassword,teacherID,favourites){
+			super(username,upassword);
+			this.teacherID=teacherID;
+			this.favourites=favourites;
+	}
+	addfav(fav){
+		this.favourites.push(fav);
+	}
+}
+class teacher extends User{
+		constructor(username,upassword,teacherID){
+			super(username,upassword);
+			this.teacherID=teacherID;
+			this.students=[];
+			this.stages=[];
+	}
+	addstudent(student){
+		this.students.push(student);
+	}
+	addstage(stage){
+		this.stages.push(stage);
+	}
+}
+class CardSet{
+	constructor(topic,cardlist){
+		this.topic=topic;
+		this.cardlist=cardlist;
+	}
+	drawhand(num){
+		var ran;
+		var hand=[];
+		for (var i=0; i<num; i++){
+			ran = Math.floor(Math.random() * this.cardlist.length);
+			hand.push(this.cardlist[ran]);
+			console.log(this.cardlist);
+			this.cardlist.splice(ran,1);
+		}
+		return hand;
+	}
+}
+class game{
+	constructor(student,cardset,pairs,timer){
+		this.student=student;
+		this.pairs=pairs;
+		this.matches=0;
+		this.hand=cardset.drawhand;
+		this.cards=[];
+		this.score=0;
+		this.combo=0;
+		this.timer=timer;
+	}
+	createcards(){
+		for(i=0;i<pairs;i++){
+			this.cards.push(this.hand[i].articlecard());
+			this.cards.push(this.hand[i].imageecard());
+		}
+	}
+	matchscore(){
+		this.score+=combo;
+		this.combo++;
+	}
+	resetcombo(){
+		this.combo=1;
+	}
+	checkmatch(a,b){
+		if(cards[a][0]==cards[b][0]){
+			matchscore();
+			this.matches++;
+			if (matches==pairs){
+				return 2;
+			} else{
+				return 1;
+			}
+		} else{
+			return 0;
+		}
+	}
+	timedown(){
+		timer=timer-1;
+		if(timer==0){
+			return 1;
+		} else{
+			timestop();
+			return 0;
+		}
+	}
+	timerun(){
+		var mytime = setInterval(timedown(), 1000);
+	}
+	timestop(){
+		clearInterval(mytime);
+	}
+}
+
+
+
+/****//****//****//****//****//****//****//****//****//****//****//***/
+///////////////////////////////////////////////////////////////////////
+/****//****//****//****//****//****//****//****//****//****//****//***/
+	var keywordcards=[];
+	var loadedArt = [];
+	var loadedImages = [];
+	var loadedEx = [];
+    var urlPatterns = ["flickr.com", "nla.gov.au", "artsearch.nga.gov.au", "recordsearch.naa.gov.au", "images.slsa.sa.gov.au"];
+    var found = 0;
+	var artic;
+	var searchZone = "newspaper";
+	var count=0;
+	var cardnum=0;
+	var newcard;
+	var loadCard;
+	var setcards = [];
+	var num=5;
+	var json;
+	var check = 0;
+	var useid;
+(function($){
+	
+	function getCard(teachID){
+			$.ajax({
+				url:"cardget.php",
+				type: "POST",
+				datatype: "JSON",
+				data: {teacherID: teachID},
+				success: function(data) {
+					json = JSON.parse(data);
+					for (var i=0; i<json.length; i++){
+							loadCard = new Card(json[i][0],json[i][2],json[i][4],json[i][3]);
+							setcards.push(loadCard);
+					}
+				},
+				error: function() {
+					console.log("an error occured");
+				}
+			});
+			json = new CardSet("politics",setcards);
+			json.drawhand(3)
+			return json;
+	}
+	
+	$("form#Test").submit(function(event) {
+		event.preventDefault();
+		useid = parseInt($("#idnum").text());
+		json = getCard(1);
+		for (var i=0;i<json.cardset.length; i++){
+			var image = new Image();
+			image.src = json.cardset[i].imageurl;
+			image.style.display = "inline-block";
+			image.style.width = "10%";
+			image.style.margin = "1%";
+			image.style.verticalAlign = "top";
+			$("#"+2*i).append(image);
+			$("#"+2*i).append(json.cardset[i].excerpt);
+		}
+	});
+	
+ 
+}
+
+  function printMultiImages(num) {
+
+			for (i=0;i<num;i++){
+				var x=1;
+				var image = new Image();
+				if (loadedImages[i] != undefined){
+					image.src = loadedImages[i];
+					image.style.display = "inline-block";
+					image.style.width = "90%";
+					image.style.margin = "1%";
+					image.style.verticalAlign = "top";
+					$("#Images").append(" <label><input type='radio' name='cardimage' value='"+i+"'><div class = '"+i+"'id='outputImg'></div><label/>");
+					$("."+i).append(image);
+				}
+			}
+        }
+
+	
+	function printCardList(list){
+		cards = list.cardlist;
+		for (i=0;i<cards.length;i++){
+			var x=1;
+			var image = new Image();
+			image.src = cards[i].imgurl;
+			image.style.display = "inline-block";
+			image.style.width = "90%";
+			image.style.margin = "1%";
+			image.style.verticalAlign = "top";
+			$("#Images").append(" <label><input type='radio' name='cardimage' value='"+i+"'><div class = '"+i+"'id='outputImg'></div><label/>");
+			$("."+i).append(image);
+			$("#Articles").append(" <label><input type='radio' name='cardart' value='"+i+"'><div class = 'A"+i+"'id='outputArt'></div><label/>");
+			$(".A"+count).append(cards[i].excerpt);
+		}
+	}
+    
+    }(jQuery));
+
